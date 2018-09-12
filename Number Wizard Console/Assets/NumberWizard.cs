cafc942d6a6f4110467c3999e87d7d55f2e4817d
wcalvert88 <wcalvert88@gmail.com>
2018-09-12 15:49:30 -0400

    made it a little dynamic and added variables for the min and max values

--- Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -6,10 +6,12 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        int max = 1000;
+        int min = 1;
         Debug.Log("Welcome to number wizard!");
         Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is 1000.");
-        Debug.Log("The lowest number you can pick is 1.");
+        Debug.Log("The highest number you can pick is: " + max + ".");
+        Debug.Log("The lowest number you can pick is: " + min + ".");
     }
 	
 	// Update is called once per frame
