cde5325b29bed51f3b713ae5e2aea8af04729c2c
wcalvert88 <wcalvert88@gmail.com>
2018-09-12 17:34:17 -0400

    finished guess logic

--- Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -11,31 +11,31 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
 
 
-        Debug.Log("Welcome to number wizard!");
-        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("Welcome to the number wizard!");
+        Debug.Log("Pick a number, don't tell me what it is!");
         Debug.Log("The highest number you can pick is: " + max + ".");
         Debug.Log("The lowest number you can pick is: " + min + ".");
-        Debug.Log("Tell me if your number is higher or lower than " + guess + ".");
+        Debug.Log("Tell me if your number is higher or lower than: " + guess + ".");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("I guessed too low! Is it higher or lower than: " + guess);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
             max = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("I guessed too high! Is it higher or lower than: " + guess);
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter key was pressed.");
             Debug.Log("I got the number correct!");
         }
     }
