b0738acbcf92016086fc205ffab087b555ad80a1
wcalvert88 <wcalvert88@gmail.com>
2018-09-12 17:12:01 -0400

    moved variables to class level to use it in both methods

--- Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
 
 	// Use this for initialization
 	void Start () {
-        int max = 1000;
-        int min = 1;
+
+
         Debug.Log("Welcome to number wizard!");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max + ".");
         Debug.Log("The lowest number you can pick is: " + min + ".");
-        Debug.Log("Tell me if your number is higher or lower than 500.");
+        Debug.Log("Tell me if your number is higher or lower than " + guess + ".");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 	
@@ -21,14 +24,19 @@ public class NumberWizard : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Enter key was pressed.");
+            Debug.Log("I got the number correct!");
         }
     }
 }
